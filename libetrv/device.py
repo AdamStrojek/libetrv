0a5aaf9c761c7ee9ae608099eff352e1d25f7f74
Adam Strojek <adam@strojek.info>
2019-04-21 22:42:18 +0200

    Changes to device.py file

--- libetrv/device.py
+++ libetrv/device.py
@@ -1,6 +1,8 @@
 from time import sleep
 
+import xxtea
 from bluepy import btle
+from loguru import logger
 
 
 class eTRVDevice(object):
@@ -12,20 +14,26 @@ class eTRVDevice(object):
 
     SECRET_R = 0x003f
 
-    def __init__(self, address, secret=None):
+    def __init__(self, address, secret=None, pin=None):
         """
         Constructor for eTRVDevice
         """
         self.address = address
         self.secret = secret
-        self.pin = b'0000'
+        self.pin = b'0000' if pin is None else pin
         self.ble_device = None  # type: btle.Peripheral
 
     def is_connected(self):
         return self.ble_device is not None
 
-    def connect(self):
+    def connect(self, send_pin=True):
+        """
+        This method allow you to connect to eTRV device and if it is required it will
+        also send pin. You can select is it necessery
+        """
+        logger.debug("Trying connect to {}", self.address)
         if self.is_connected():
+            logger.debug("Device already connected {}", self.address)
             return
 
         while True:
@@ -33,22 +41,60 @@ class eTRVDevice(object):
                 self.ble_device = btle.Peripheral(self.address)
                 self.__send_pin()
                 break
-            except btle.BTLEDisconnectError as exc:
+            except btle.BTLEDisconnectError:
+                logger.error("Unable connect to {}. Retrying in 100ms", self.address)
                 sleep(0.1)
 
     def disconnect(self):
+        logger.debug("Disconnecting")
         if self.ble_device is not None:
             self.ble_device.disconnect()
+            self.ble_device = None
 
     def __send_pin(self):
+        logger.debug("Write PIN to {}", self.address)
         self.ble_device.writeCharacteristic(eTRVDevice.PIN_W, self.pin, True)
+
+    def __write(self, service: int, data: bytes):
+        if not self.is_connected():
+            self.connect()
+
+        data = data[::-1]
+        res = self.ble_device.writeCharacteristic(service, data, True)  # type: bytes
+        return res[::-1]
+
+    def __read(self, service: int):
+        if not self.is_connected():
+            self.connect()
+
+        res = self.ble_device.readCharacteristic(service)  # type: bytes
+        # res = res[::-1]
+        xxtea.decrypt(res, self.secret, padding=False,rounds=32)
+        return res
 
     def get_encryption_key(self):
         pass
 
     @property
+    def pin(self):
+        """The pin property."""
+        return self._pin
+
+    @pin.setter
+    def pin(self, value):
+        self._pin = value
+
+    @property
+    def secret(self):
+        """The secret property."""
+        return self._secret
+    @secret.setter
+    def secret(self, value):
+        self._secret = value
+
+    @property
     def temperature(self):
-        return None
+        return self.__read(eTRVDevice.MANUAL_TEMPERATURE_RW)
 
     @temperature.setter
     def set_temperature(self, value):
