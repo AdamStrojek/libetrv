d1d274937fb7fdac538c532c9975f37a3710456e
Adam Strojek <adam@strojek.info>
2019-04-22 23:13:47 +0200

    Update utils, move encrypt and repack functions

--- libetrv/utils.py
+++ libetrv/utils.py
@@ -1,4 +1,8 @@
+import struct
 from functools import wraps
+
+import xxtea
+
 
 def etrv_read(handler, send_pin=False):
     def decorator(func):
@@ -10,3 +14,18 @@ def etrv_read(handler, send_pin=False):
             return func(etrv, data)
         return wrapper
     return decorator
+
+
+def etrv_repack(data: bytes, format: str):
+    return struct.pack(format, *struct.unpack('>'+format, data))
+
+
+def etrv_decode(data: bytes, key: bytes):
+    return xxtea.decrypt(data, key, padding=False, rounds=32)
+
+
+def etrv_reverse_chunks(data: bytes):
+    result = []
+    for i in range(0, len(data), 4):
+        result.append(data[i:i+4][::-1])
+    return b''.join(result)
