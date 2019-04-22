22070451c06ceb68e2983044e8a57ce4b14dcef2
Adam Strojek <adam@strojek.info>
2019-04-22 15:24:54 +0200

    Move retrive key to device section

--- libetrv/cli.py
+++ libetrv/cli.py
@@ -5,22 +5,15 @@ from libetrv.device import eTRVDevice
 class CLI:
     def __init__(self, pin=b'0000', secret=None):
         self._pin = pin
-        self._secret = secret
-        self.temperature = Temperature(pin, secret)
+        if secret is not None:
+            self._secret = bytes.fromhex(secret)
+        else:
+            self._secret = None
 
     def scan(self, timeout=10.):
         print("Detected eTRV devices:")
         for device in eTRVDevice.scan(timeout):
             print("{}, RSSI={}dB".format(device.addr, device.rssi))
-
-    def retrive_key(self, device):
-        print(
-            "In 5 seconds this script will try to retrieve a secure key from eTRV device. "
-            "Don't forget to save it for later. Before that be sure that device is in pairing mode. "
-            "You can achieve that by pressing button on device"
-        )
-        time.sleep(5)
-        print("Secret Key:", eTRVDevice(device, pin=self._pin).retrieve_secret_key().hex())
     
     def device(self, device_id):
         return Device(device_id, self._pin, self._secret)
@@ -31,7 +24,16 @@ class Device:
         self._pin = pin
         self._secret = secret
         self._device = eTRVDevice(device, pin=self._pin, secret=self._secret)
-    
+
+    def retrive_key(self):
+        print(
+            "In 5 seconds this script will try to retrieve a secure key from eTRV device. "
+            "Don't forget to save it for later. Before that be sure that device is in pairing mode. "
+            "You can achieve that by pressing button on device"
+        )
+        time.sleep(5)
+        print("Secret Key:", self._device.retrieve_secret_key())
+
     def battery(self):
         battery = self._device.battery
         print("Battery level: {}%".format(battery))
