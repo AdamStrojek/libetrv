222b568e45e6497b629bc82ba717a97f0a29980c
Adam Strojek <adam@strojek.info>
2019-04-26 15:19:06 +0200

    Initial setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+
+setup(name='libetrv',
+      version='0.1dev',
+      description='Monitor and control your eTRV from Python',
+      author='Adam Strojek',
+      author_email='adam@strojek.info',
+      packages=['libetrv', ],
+     )
