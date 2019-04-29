2fa0e5f90906d3fbc3e53d2db0d3fd69e8609986
Adam Strojek <adam@strojek.info>
2019-04-29 12:23:24 +0200

    Update unit tests for schedule

--- tests/unit/test_schedule.py
+++ tests/unit/test_schedule.py
@@ -8,13 +8,13 @@ from libetrv.schedule import Schedule
 def data():
     return bytes([
         44, 37, # home, away temperature
-        0, 10, 30, 48, 0, 0,  # monday
-        0, 10, 30, 48, 0, 0,  # tuesday
-        0, 10, 30, 48, 0, 0,  # wednesday
-        0, 10, 30, 48, 0, 0,  # thursday
-        0, 10, 30, 48, 0, 0,  # friday
-        0, 10, 30, 48, 0, 0,  # saturday
-        0, 10, 30, 48, 0, 0,  # sunday
+        0, 10, 31, 48, 0, 0,  # monday
+        0, 10, 31, 48, 0, 0,  # tuesday
+        0, 10, 31, 48, 0, 0,  # wednesday
+        0, 10, 31, 48, 0, 0,  # thursday
+        0, 10, 31, 48, 0, 0,  # friday
+        0, 10, 31, 48, 0, 0,  # saturday
+        0, 10, 31, 48, 0, 0,  # sunday
     ])
 
 
@@ -33,7 +33,7 @@ class TestScheduleStruct:
         assert obj.away_temperature == 37
         assert len(obj.schedule) == 7
         for i in range(7):
-            assert obj.schedule[i].data == [0, 10, 30, 48, 0, 0]
+            assert obj.schedule[i].data == [0, 10, 31, 48, 0, 0]
 
 
 class TestScheduleParsing:
@@ -41,3 +41,10 @@ class TestScheduleParsing:
         obj = Schedule.from_struct(data_struct)
         assert obj.home_temperature == 22
         assert obj.away_temperature == 18.5
+        assert len(obj.schedule) == 7
+        for i in range(7):
+            assert len(obj.schedule[i]) == 4
+            assert obj.schedule[i][0] == (False, 0, 0)
+            assert obj.schedule[i][1] == (True, 5, 0)
+            assert obj.schedule[i][2] == (False, 15, 30)
+            assert obj.schedule[i][3] == (True, 24, 0)
