89a6613bfe4be36e22be77e0259c292f1efcb39d
Adam Strojek <adam@strojek.info>
2019-04-29 11:28:36 +0200

    Switch method to classmethod

--- libetrv/schedule.py
+++ libetrv/schedule.py
@@ -13,9 +13,12 @@ class Schedule:
         self.away_temperature = None
         self.schedule = [[]*7]
     
-    def parse_struct(self, data: ScheduleStruct):
-        self.home_temperature = data.home_temperature * .5
-        self.away_temperature = data.away_temperature * .5
+    @classmethod
+    def from_struct(cls, data: ScheduleStruct):
+        obj = cls()
+        obj.raw_data = data
+        obj.home_temperature = data.home_temperature * .5
+        obj.away_temperature = data.away_temperature * .5
         for weekday, schedule in enumerate(data.schedule):
             is_away = False
             for raw_time in schedule:
@@ -24,6 +27,7 @@ class Schedule:
                 hours, halfs = divmod(raw_time, 2)
                 item.time = time(hours, [0, 30][halfs])
                 is_away = not is_away
-                self.schedule[weekday].append(item)
+                obj.schedule[weekday].append(item)
                 if hours >= 24:
                     break
+        return obj
