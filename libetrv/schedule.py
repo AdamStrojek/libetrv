1f4466a08806d827726a473c5ee2401af03f0c05
Adam Strojek <adam@strojek.info>
2019-04-29 14:06:46 +0200

    Allow to fix times

--- libetrv/schedule.py
+++ libetrv/schedule.py
@@ -1,9 +1,22 @@
 from collections import namedtuple
 
 from .data_struct import ScheduleStruct
+from .exceptions import ParsingError
 
 
 TimeSchedule = namedtuple("TimeSchedule", ['is_away', 'hour', 'minute'])
+
+
+def fix_raw_time(raw_time: int, fail_silently: bool = True) -> int:
+    if raw_time < 0:
+        result = 0
+    elif raw_time > 48:
+        result = 48
+    
+    if not fail_silently and result != raw_time:
+        raise ParsingError("raw_time exeeded limit")
+    
+    return result
 
 
 class Schedule:
@@ -15,7 +28,7 @@ class Schedule:
             self.schedule.append([])
 
     @classmethod
-    def from_struct(cls, data: ScheduleStruct):
+    def from_struct(cls, data: ScheduleStruct, fail_silently: bool = True):
         obj = cls()
         obj.raw_data = data
         obj.home_temperature = data.home_temperature * .5
@@ -23,6 +36,7 @@ class Schedule:
         for weekday, schedule in enumerate(data.schedule):
             is_away = False
             for raw_time in schedule.data:
+                raw_time = fix_raw_time(raw_time, fail_silently)
                 hour, half = divmod(raw_time, 2)
                 item = TimeSchedule(is_away, hour, [0, 30][half])
                 is_away = not is_away
