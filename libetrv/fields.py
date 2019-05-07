bd528bf81aabe8eacd59d34111e31082a6be40da
Adam Strojek <adam@strojek.info>
2019-05-07 17:21:26 +0200

    New fields responsible for proper saving values in structures

---
+++ libetrv/fields.py
@@ -0,0 +1,55 @@
+from datetime import datetime, timedelta, timezone
+from .properties import eTRVField
+
+
+class TemperatureField(eTRVField):
+    def from_raw_value(self, raw_value, property):
+        return raw_value * .5
+
+    def to_raw_value(self, value, property):
+        return int(value*2)
+
+
+class UTCDateTimeField(eTRVField):
+    def from_raw_value(self, raw_value, property):
+        if raw_value == 0:
+            return None
+        return datetime.utcfromtimestamp(raw_value)
+
+    def to_raw_value(self, value, property):
+        if value is None:
+            return 0
+        elif isinstance(value, datetime):
+            return int(value - datetime(1970, 1, 1, tzinfo=timezone.utc)).total_seconds()
+        elif isinstance(value, int):
+            return value
+        
+        raise ValueError('type not supported')
+
+
+class LocalDateTimeField(eTRVField):
+    def __init__(self, tz_field, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.tz_field = tz_field
+
+    def from_raw_value(self, raw_value, property):
+        if raw_value == 0:
+            return None
+        return datetime.fromtimestamp(
+            raw_value,
+            tz=timezone(timedelta(seconds=getattr(property, self.tz_field))
+        ))
+
+    def to_raw_value(self, value, property):
+        setattr(property, self.tz_field, 0)
+        if value is None:
+            return 0
+        elif isinstance(value, datetime):
+            utc_offset = value.utcoffset()
+            if utc_offset is not None:
+                setattr(property, self.tz_field, utc_offset.total_seconds())
+            return value.timestamp()
+        elif isinstance(value, int):
+            return value
+        
+        raise ValueError('type not supported')
