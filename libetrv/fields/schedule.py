d56d0641a79c61a79f7e45aac38853ec49dc5fee
Adam Strojek <adam@strojek.info>
2019-05-18 13:52:13 +0200

    Refactoring schedule fields

---
+++ libetrv/fields/schedule.py
@@ -0,0 +1,32 @@
+from collections import namedtuple
+
+from .base import eTRVField
+
+
+TimeSchedule = namedtuple("TimeSchedule", ['is_away', 'hour', 'minute'])
+
+
+def fix_raw_time(raw_time: int) -> int:
+    result = max(0, min(48, raw_time))
+    return result
+
+
+class DailyScheduleField(eTRVField):
+    def from_raw_value(self, raw_value, property):
+        items = []
+        is_away = False
+
+        if raw_value[0] != 0:
+            items.append(TimeSchedule(is_away, 0, 0))
+            is_away = not is_away
+
+        for raw_time in raw_value:
+            raw_time = fix_raw_time(raw_time)
+            hour, half = divmod(raw_time, 2)
+            item = TimeSchedule(is_away, hour, [0, 30][half])
+            is_away = not is_away
+            items.append(item)
+            if raw_time >= 48:
+                break
+        
+        return items
