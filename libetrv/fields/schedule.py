2a30721c5febc15bd32a15bc0db5b84434459e7b
Adam Strojek <adam@strojek.info>
2019-10-14 13:48:02 +0200

    Update code and add documentations

--- libetrv/fields/schedule.py
+++ libetrv/fields/schedule.py
@@ -1,5 +1,5 @@
 from collections import namedtuple
-from typing import List
+from typing import List, Generator
 
 from .base import eTRVField
 
@@ -13,23 +13,29 @@ def fix_raw_time(raw_time: int) -> int:
 
 
 class DailyScheduleField(eTRVField):
-    def from_raw_value(self, raw_value, property):
-        items = []
-        is_away = False
-
-        for raw_time in raw_value:
+    def from_raw_value(self, raw_value, property) -> Generator[TimeSchedule]:
+        """
+        RAW data from device have it own format. It is array of 6 short integers (char),
+        by default 0 filled. Each value is saving information about hour and half. To
+        calculate proper hour you need to divide it by 2, all halfs are 30 minutes.
+        Each odd item starts home temperature.
+        Each even item starts away temperature.
+        First record do not need to start from midnight, it can be any hour. All values
+        above 48 are not possible to assigne, because this is above 24h clock.
+        """
+        for index, raw_time in enumerate(raw_value):
             raw_time = fix_raw_time(raw_time)
             hour, half = divmod(raw_time, 2)
-            item = TimeSchedule(is_away, hour, [0, 30][half])
-            is_away = not is_away
-            items.append(item)
+            yield TimeSchedule(bool(index/2), hour, [0, 30][half])
             if raw_time >= 48:
                 break
-        
-        return items
 
     def to_raw_value(self, value: List[TimeSchedule], property):
+        """
+        This method will totally ignore `is_away` attribute
+        """
         result = [0]*6
+        value = value[:6]  # More then 6 values will be ignored
         for i, entry in enumerate(value):
             result[i] = entry.hour*2 + int(entry.minute >= 30)
         return result
